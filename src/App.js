3c26f97eae3a0691c5775e2c115f686f74fb476d
Carter Chen <carterchen@Carters-MacBook-Pro.local>
2020-10-10 15:48:00 -0400

    added build:extension scripts, manifest.json boilerplate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+console.log("hello world");
 
 function App() {
   return (
