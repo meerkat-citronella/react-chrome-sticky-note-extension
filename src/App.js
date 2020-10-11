f6ad450c84700e411ff841bc7ebd83e98f7f8d43
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2020-10-11 01:33:55 -0400

    added delete, styling

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ import "./App.css";
 const StyledTextArea = styled.textarea`
   height: 200px;
   width: 200px;
+  border: none;
+`;
+
+const StyledButton = styled.button`
+  height: 20px;
+  border: none;
+  opacity: 0.5;
+  float: right;
+`;
+
+const Container = styled.div`
   border: 1px solid grey;
   position: absolute;
   top: ${(props) => props.y + "px"};
   left: ${(props) => props.x + "px"};
+`;
+
+const Header = styled.div`
+  height: 20px;
+  background: linear-gradient(to right, #b3d279, #bf7340);
 `;
 
 const App = () => {
@@ -22,8 +38,8 @@ const App = () => {
         setNotes((prevNotes) => [...prevNotes, { x: e.pageX, y: e.pageY }]);
       }
     }
-    document.addEventListener("click", clickListener, false);
-    return () => document.removeEventListener("click", clickListener, false);
+    document.addEventListener("click", clickListener);
+    return () => document.removeEventListener("click", clickListener);
   }, []);
 
   return (
@@ -32,19 +48,34 @@ const App = () => {
         const handleChange = (e) => {
           const editedText = e.target.value;
           setNotes((prevNotes) =>
-            prevNotes.reduce((acc, cv) => {
-              if (cv.x === note.x && cv.y === note.y) {
-                acc.push({ ...cv, note: editedText });
-                return acc;
-              } else {
-                acc.push(cv);
-                return acc;
-              }
-            }, [])
+            prevNotes.reduce(
+              (acc, cv) =>
+                cv.x === note.x && cv.y === note.y
+                  ? acc.push({ ...cv, note: editedText }) && acc
+                  : acc.push(cv) && acc,
+              []
+            )
           );
         };
 
-        return <StyledTextArea x={note.x} y={note.y} onChange={handleChange} />;
+        const handleDelete = () => {
+          setNotes((prevNotes) =>
+            prevNotes.reduce(
+              (acc, cv) =>
+                cv.x === note.x && cv.y === note.y ? acc : acc.push(cv) && acc,
+              []
+            )
+          );
+        };
+
+        return (
+          <Container x={note.x} y={note.y}>
+            <Header>
+              <StyledButton onClick={handleDelete}>X</StyledButton>
+            </Header>
+            <StyledTextArea onChange={handleChange} />
+          </Container>
+        );
       })}
     </div>
   );
