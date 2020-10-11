3a26744ddda2e175131cda79b934ec82dc8d1355
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2020-10-10 22:44:34 -0400

    notes working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import "./App.css";
 
-console.log("hello world");
+// const sampleNotesShape = [{ x: 98, y: 836, note: "some note text" }];
 
-function App() {
+const StyledTextArea = styled.textarea`
+  height: 200px;
+  width: 200px;
+  border: 1px solid grey;
+  position: absolute;
+  top: ${(props) => props.y + "px"};
+  left: ${(props) => props.x + "px"};
+`;
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    function clickListener(e) {
+      if (e.shiftKey) {
+        setNotes((prevNotes) => [...prevNotes, { x: e.pageX, y: e.pageY }]);
+      }
+    }
+    document.addEventListener("click", clickListener, false);
+    return () => document.removeEventListener("click", clickListener, false);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {notes.map((note) => {
+        const handleChange = (e) => {
+          const editedText = e.target.value;
+          setNotes((prevNotes) =>
+            prevNotes.reduce((acc, cv) => {
+              if (cv.x === note.x && cv.y === note.y) {
+                acc.push({ ...cv, note: editedText });
+                return acc;
+              } else {
+                acc.push(cv);
+                return acc;
+              }
+            }, [])
+          );
+        };
+
+        return <StyledTextArea x={note.x} y={note.y} onChange={handleChange} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
