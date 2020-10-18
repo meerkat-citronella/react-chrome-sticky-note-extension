d6a59193fb2a3dfefe1a708a281bffb9ffcef824
areibman <meta.alex.r@gmail.com>
2020-10-18 18:31:57 -0400

    working solution

--- src/index.js
+++ src/index.js
@@ -15,6 +15,16 @@ ReactDOM.render(
   document.getElementById("insertion-point")
 );
 
+
+// For the popup
+ReactDOM.render(
+    <React.Fragment>
+      <h1>IT WORKED!</h1>
+      <div style={{width:'400px', height:'200px', backgroundColor:"green"}}/>
+    </React.Fragment>,
+  document.getElementById("root")
+);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
